1771e24e169aae4137d758857518078f642ae3bc
Yaakov <yasha0x80@gmail.com>
2016-10-04 23:41:49 +0300

    Update rserver.rb

--- rserver.rb
+++ rserver.rb
@@ -1,11 +1,9 @@
 require 'webrick'
 
-#root = File.expand_path 'www'
 puts "[*] you're running at http://localhost:8080"
-server = WEBrick::HTTPServer.new :Port => 8080, :DocumentRoot => '/Users/sergatamobile/Desktop/nooly 2/www'
+server = WEBrick::HTTPServer.new Port: 8080, DocumentRoot: Dir.pwd
 
 trap 'INT' do
-  puts "shuting down................"
   server.shutdown
   puts "server is down!"
 end
